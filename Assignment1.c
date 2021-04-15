af4eedaf0a09591918c8e166c2bc674dd69c1b48
Mariam Amr <mariamamryoussef@gmail.com>
2021-04-15 16:32:22 +0200

    first commit

---
+++ Assignment1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+struct Student
+{
+    char name[100];
+    int std_ID;
+    int day,month,year;
+    int score_std;
+};
+int main()
+{
+    printf("Welcome\n");
+    return 0;
+}
