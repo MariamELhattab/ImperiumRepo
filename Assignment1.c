937b764bf219f5c6bbeefca9351ff1c4d95e28b7
Amr Maged <amrmaged8770@gmail.com>
2021-07-09 13:15:52 +0200

    removed array

--- Assignment1.c
+++ Assignment1.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <assert.h>
 
-//N : number of students
-int N=5;
-
 struct Student
 {
 	char name[100];
@@ -15,11 +12,6 @@ struct Student
     struct Student *next;
 };
 
-void insertInTheBeginning_DA(struct Student *stdPtr);
-void insertAtTheEnd_DA(struct Student *stdPtr);
-void insertInTheMiddle_DA(struct Student *stdPtr, int location);
-void insert (struct Student *stdPtr);
-void showArray(struct Student *stdPtr);
 void InsertFirst_LL (void);
 void InsertEnd_LL (void);
 void InsertAtPos_LL (int pos);
@@ -33,136 +25,7 @@ int length = 0;//Variable to know the size of the list
 
 int main()
 {
-	printf("Welcome\n");
-
-	struct Student *stdPtr;
-
-	stdPtr = (struct Student *) malloc(N*sizeof(struct Student));
-
-	return 0;
-}
-
-
-void insertInTheBeginning_DA(struct Student *stdPtr)
-{
-	int i;
-
-	for (i=0; i<N;i++)
-	{
-		if ((stdPtr+i)->flag != 1)
-		{
-			break;
-		}
-	}
-
-	assert(i!=N);
-
-	for (; i>0 ;i--)
-	{
-		*(stdPtr+i)=*(stdPtr+i-1);
-		(stdPtr+i-1)->flag=0;
-	}
-	insert (stdPtr);
-}
-
-void insertAtTheEnd_DA(struct Student *stdPtr)
-{
-	int i;
-
-	for (i=N-1; i>= 0 ;i--)
-	{
-		if ((stdPtr+i)->flag != 1)
-		{
-			break;
-		}
-	}
-
-	assert(i!=-1);
-
-	for (;i<N-1;i++)
-	{
-		*(stdPtr+i)=*(stdPtr+i+1);
-		(stdPtr+i+1)->flag=0;
-	}
-	insert (stdPtr+N-1);
-}
-
-void insertInTheMiddle_DA(struct Student *stdPtr, int location)
-{
-	int i;
-
-	if ((stdPtr+location-1)->flag == 1)
-	{
-		for (i=0; i<N;i++)
-		{
-			if ((stdPtr+i)->flag != 1)
-			{
-				break;
-			}
-		}
-
-		assert(i!=N);
-
-		if (i<location-1)
-		{
-			for (;i<N-1;i++)
-			{
-				*(stdPtr+i)=*(stdPtr+i+1);
-				(stdPtr+i+1)->flag=0;
-			}
-		}
-		else
-		{
-			for (; i>0 ;i--)
-			{
-				*(stdPtr+i)=*(stdPtr+i-1);
-				(stdPtr+i-1)->flag=0;
-			}
-		}
-	}
-	insert (stdPtr+(location-1));
-}
-
-void insert (struct Student *stdPtr)
-{
-
-	printf("Enter the student name: ");
-	fflush(stdin); fflush(stdout);
-	gets(stdPtr->name);
-
-	printf("Enter the student ID: ");
-	fflush(stdout);
-	scanf("%d",&(stdPtr->std_ID));
-
-	printf("Enter the student birth date\nyear: ");
-	fflush(stdout);
-	scanf("%d",&stdPtr->year);
-	printf("month: ");
-	fflush(stdout);
-	scanf("%d",&stdPtr->month);
-	printf("day: ");
-	fflush(stdout);
-	scanf("%d",&stdPtr->day);
-
-	printf("Enter the student score: ");
-	fflush(stdout);
-	scanf("%d",&stdPtr->score_std);
-
-	stdPtr->flag=1;
-}
-
-void showArray(struct Student *stdPtr)
-{
-	int i=0;
-
-	for (i=0;i<N;i++)
-	{
-		printf("[%d] std data:\n",i);
-		printf("Name: %s\n",(stdPtr+i)->name);
-		printf("ID: %d\n",(stdPtr+i)->std_ID);
-		printf("Birthdate: %d,%d,%d\n",(stdPtr+i)->day,(stdPtr+i)->month,(stdPtr+i)->year);
-		printf("Score: %d\n\n",(stdPtr+i)->score_std);
-	}
+    return 0;
 }
 
 void InsertFirst_LL (void){
@@ -181,6 +44,9 @@ void InsertFirst_LL (void){
     scanf("%d",&NewStudent->year);
     printf("Enter the score: ");
     scanf("%d",&NewStudent->score_std);
+
+    //Changing Head Pointer
+
         if (length == 0){//If the list is empty
             Head = NewStudent;
             NewStudent->next = NULL;
@@ -190,11 +56,14 @@ void InsertFirst_LL (void){
             NewStudent->next = Head;
             Head = NewStudent;
         }
+
     length ++;
+
 }
 
 void InsertEnd_LL (void){
     struct Student *NewStudent = (struct Student *) malloc (sizeof(struct Student));
+
     //Assigning the wanted data to the new Student
     printf("Enter the name: ");
     fflush(stdin);
@@ -210,6 +79,7 @@ void InsertEnd_LL (void){
     printf("Enter the score: ");
     scanf("%d",&NewStudent->score_std);
 
+    //Changing Tail and Head Pointers if needed
     if (length == 0){//To handle beginning the insertion at the end (list is empty)
         Head = Tail = NewStudent;
         NewStudent->next = NULL;
@@ -226,7 +96,9 @@ void InsertEnd_LL (void){
         Tail->next = NewStudent;
         Tail=NewStudent;
     }
+
     length ++;
+
 }
 
 void InsertAtPos_LL (int pos){
@@ -263,7 +135,9 @@ void InsertAtPos_LL (int pos){
             NewStudent->next = current->next;
             current->next = NewStudent;
     }
+
     length ++;
+
 }
 
 void PrintList (void) {
